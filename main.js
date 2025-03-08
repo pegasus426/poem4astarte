dde1b2e6f4cb0207c437237c67dddcdd2e1e1a70
Stefano Gargiulo <stefano.gargiulo.426@gmail.com>
2025-03-08 10:38:33 +0100

    temp fix letters

--- main.js
+++ main.js
@@ -90,6 +90,11 @@ function analyzePoetry() {
     // Assegna una lettera a ogni rima unica (a, b, c, ...)
     uniqueRhymes.forEach((rhyme, index) => {
         const letter = String.fromCharCode(97 + index); // 97 è il codice ASCII per 'a'
+        //se la lettera è superiore a z, assegna un simbolo leggibile
+        if (letter > 'z') {
+            letter = String.fromCharCode(65 + (letter.charCodeAt(0) - 97)); // 65 è il codice ASCII per 'A'
+        }
+        // Crea lo schema per la rima
         rhymeScheme[rhyme] = letter;
         rhymeColors[letter] = stringToColor(rhyme);
     });
